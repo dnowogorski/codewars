fd672185d518a1aa3596afa6b8cef8ae32e8db25
Dawid Nowogorski <Dawid.Nowogorski@grandparade.co.uk>
2018-01-09 13:46:23 +0100

    Add first implementation

--- src/main/java/com/dnowogorski/DashatizeIt.java
+++ src/main/java/com/dnowogorski/DashatizeIt.java
@@ -6,6 +6,24 @@ package com.dnowogorski;
 public class DashatizeIt {
 
     public static String dashatize(int num) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        String stringValue = String.valueOf(num);
+        result.append(stringValue.charAt(0));
+        final boolean[] wasPreviousOdd = {false};
+        stringValue.chars().skip(1).limit(stringValue.length() - 2L).forEach(n -> {
+            if (n % 2 != 0) {
+                if (!wasPreviousOdd[0]) {
+                    result.append("-");
+                }
+                result.append((char) n);
+                result.append("-");
+                wasPreviousOdd[0] = true;
+            } else {
+                result.append((char) n);
+                wasPreviousOdd[0] = false;
+            }
+        });
+        result.append(stringValue.charAt(stringValue.length() - 1));
+        return result.toString();
     }
 }
