82c6728a33a0d1bc31cde0d315f8f1094a957cce
Dawid Nowogorski <Dawid.Nowogorski@grandparade.co.uk>
2018-01-09 12:22:54 +0100

    Add basic test

--- src/test/groovy/com/dnowogorski/DashatizeItTest.groovy
+++ src/test/groovy/com/dnowogorski/DashatizeItTest.groovy
@@ -1,4 +1,18 @@
 package com.dnowogorski
 
-class DashatizeItTest {
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class DashatizeItTest extends Specification {
+
+    def dashatizer = new DashatizeIt()
+
+    @Unroll
+    def "should dashatize basic"() {
+        expect:
+            dashatizer.dashatize(num) == result
+        where:
+            num << [274, 5311, 86320, 974302]
+            result << ["2-7-4", "5-3-1-1", "86-3-20", "9-7-4-3-02"]
+    }
 }
