8e02e667aa941fc86daa37a41ec3df995e69d0e8
dnowogorski <reponagicie.x01>
2018-01-22 20:16:19 +0100

    Add kata reference

--- src/main/java/com/dnowogorski/HighestScoringWord.java
+++ src/main/java/com/dnowogorski/HighestScoringWord.java
@@ -3,6 +3,9 @@ package com.dnowogorski;
 import static java.util.Arrays.stream;
 import static java.util.Comparator.comparingInt;
 
+/**
+ * https://www.codewars.com/kata/57eb8fcdf670e99d9b000272
+ */
 public class HighestScoringWord {
 
     public static String high(String sentence) {
