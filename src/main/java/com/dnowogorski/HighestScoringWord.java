cff9b949973979b28342753149f06666a37793e8
dnowogorski <reponagicie.x01>
2018-01-22 20:15:49 +0100

    Add solution

--- src/main/java/com/dnowogorski/HighestScoringWord.java
+++ src/main/java/com/dnowogorski/HighestScoringWord.java
@@ -1,9 +1,14 @@
 package com.dnowogorski;
 
+import static java.util.Arrays.stream;
+import static java.util.Comparator.comparingInt;
+
 public class HighestScoringWord {
 
     public static String high(String sentence) {
-        // Your code here...
-        return "";
+        return stream(sentence.split(" "))
+                .max(comparingInt(
+                        word -> word.chars().map(i -> i - 96).sum()))
+                .get();
     }
 }
