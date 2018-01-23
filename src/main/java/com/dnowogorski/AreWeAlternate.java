0386069efa77cb66ab5112482fba5549c1c9ac2a
dnowogorski <reponagicie.x01>
2018-01-23 21:05:49 +0100

    Add reference

--- src/main/java/com/dnowogorski/AreWeAlternate.java
+++ src/main/java/com/dnowogorski/AreWeAlternate.java
@@ -1,5 +1,8 @@
 package com.dnowogorski;
 
+/**
+ * https://www.codewars.com/kata/are-we-alternate/
+ */
 public class AreWeAlternate {
     public static boolean isAlt(String word) {
         return false;
