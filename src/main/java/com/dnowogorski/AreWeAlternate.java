2207c220538684ee5a239358c0864eb9963a1e3e
dnowogorski <reponagicie.x01>
2018-01-23 21:05:14 +0100

    Add tests

--- src/main/java/com/dnowogorski/AreWeAlternate.java
+++ src/main/java/com/dnowogorski/AreWeAlternate.java
@@ -2,8 +2,6 @@ package com.dnowogorski;
 
 public class AreWeAlternate {
     public static boolean isAlt(String word) {
-        //make note that 'y' should not be treated as vowel
-        //your code here
         return false;
     }
 }
