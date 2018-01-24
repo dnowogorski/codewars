70fcafb2dd9bb683b4cd0dbe677a001b9bb03e26
dnowogorski <reponagicie.x01>
2018-01-24 21:12:54 +0100

    Add solution

--- src/main/java/com/dnowogorski/AreWeAlternate.java
+++ src/main/java/com/dnowogorski/AreWeAlternate.java
@@ -4,7 +4,19 @@ package com.dnowogorski;
  * https://www.codewars.com/kata/are-we-alternate/
  */
 public class AreWeAlternate {
+    private static final String VOWELS = "aeiou";
+
     public static boolean isAlt(String word) {
-        return false;
+        boolean wasPreviousVovel = isVovel(word.charAt(0));
+        for (int i = 1; i < word.length(); i++) {
+            boolean isCurrentVowel = isVovel(word.charAt(i));
+            if (wasPreviousVovel == isCurrentVowel) return false;
+            wasPreviousVovel = isCurrentVowel;
+        }
+        return true;
+    }
+
+    private static boolean isVovel(char c) {
+        return VOWELS.contains(c + "");
     }
 }
