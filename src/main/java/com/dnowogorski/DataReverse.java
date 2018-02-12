ae85d0b15c4fc62c72f3eca12f162708e0c9049f
dnowogorski <reponagicie.x01>
2018-02-12 20:44:15 +0100

    Final solution

--- src/main/java/com/dnowogorski/DataReverse.java
+++ src/main/java/com/dnowogorski/DataReverse.java
@@ -1,27 +1,11 @@
 package com.dnowogorski;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 public class DataReverse {
     public static int[] dataReverse(int[] data) {
-        List<List<Integer>> chunks = chunks(Arrays.stream(data).boxed().collect(Collectors.toList()));
-        Collections.reverse(chunks);
-        return Arrays.stream(chunks.stream()
-                .flatMap(List::stream)
-                .collect(Collectors.toList())
-                .stream()
-                .toArray(Integer[]::new))
-                .mapToInt(Integer::intValue).toArray();
-
-    }
-
-    private static List<List<Integer>> chunks(List<Integer> data) {
-        return IntStream.range(0, data.size() / 8)
-                .mapToObj(i -> data.subList(8 * i, Math.min(8 * i + 8, data.size())))
-                .collect(Collectors.toList());
+        int result[] = new int[data.length];
+        for (int i = data.length - 8, j = 0; i >= 0; i -= 8, j += 8) {
+            System.arraycopy(data, j, result, i, 8);
+        }
+        return result;
     }
 }
